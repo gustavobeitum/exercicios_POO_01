ddbf7df33dbd9207e12ed089b173d30bd5236e0d
gustavobeitum <guhenrique.silva04@gmail.com>
2025-06-06 23:39:27 -0300

    first commit

---
+++ src/Exercicio_1_1/Main.java
@@ -0,0 +1,45 @@
+package Exercicio_1_1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Menu menu = new Menu();
+            System.out.print("Digite o primeiro valor: ");
+            int numberOne = sc.nextInt();
+
+            System.out.print("Digite o segundo valor: ");
+            int numberTwo = sc.nextInt();
+
+            Calculadora calculo = new Calculadora(numberOne, numberTwo);
+
+            menu.showMenu();
+
+            int option = sc.nextInt();
+            menu.setOption(option);
+
+            switch (menu.getOption()) {
+                case 1:
+                    System.out.println("Resultado da soma " + calculo.sum());
+                    break;
+                case 2:
+                    System.out.println("Resultado da subtração " + calculo.subtraction());
+                    break;
+                case 3:
+                    System.out.println("Resultado da multiplicação " + calculo.multiplication());
+                    break;
+                case 4:
+                    System.out.println("Resultado da divisão " + calculo.division());
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        
+        sc.close();
+    }
+}
